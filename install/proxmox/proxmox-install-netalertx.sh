d83af0cce90d97f8856a043ace13ae946d3cf88e
Joerg Heinemann <heinemannj66@gmail.com>
2026-02-19 11:03:49 +0100

    Fix rmdir command syntax in installation script

--- install/proxmox/proxmox-install-netalertx.sh
+++ install/proxmox/proxmox-install-netalertx.sh
@@ -182,7 +182,7 @@ printf "%b\n" "-----------------------------------------------------------------
 printf "%b\n" "${GREEN}[INSTALLING]                          ${RESET}Cloning application repository and setup"
 printf "%b\n" "--------------------------------------------------------------------------"
 
-rmdir -rf "$INSTALL_DIR"
+rmdir -R "$INSTALL_DIR"
 mkdir -p "$INSTALL_DIR"
 git clone https://github.com/netalertx/NetAlertX.git "$INSTALL_DIR/"
 
